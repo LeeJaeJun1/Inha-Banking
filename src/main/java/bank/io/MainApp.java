1310c625a9f0f701b06fdc2ccbeecd8368f8289b
LeeJaeJun1 <jj030207@naver.com>
2025-06-19 16:19:19 +0900

    :sparkles: Feat: H2 Init Swing Init 추가

--- src/main/java/bank/io/MainApp.java
+++ src/main/java/bank/io/MainApp.java
@@ -1,9 +1,11 @@
 package bank.io;
 
-
+import bank.io.db.H2Database;
+import bank.io.view.LoginView;
 
 public class MainApp {
-	public static void main(String[] args) {
-
-	}
+    public static void main(String[] args) {
+        H2Database.init();
+        javax.swing.SwingUtilities.invokeLater(() -> new LoginView().show());
+    }
 }
